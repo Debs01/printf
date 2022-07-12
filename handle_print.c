3b4d8e571e5bfd8377471ab42c61ccc030571661
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-07-12 09:38:59 -0700

    Program to handle prints

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
@@ -8,8 +7,8 @@
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
